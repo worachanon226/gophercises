bdded45e08b3a87581e08415639551db8e875c84
Amaz <worachanon226@hotmail.com>
2023-01-10 16:08:46 +0700

    Update defaultHandlerTemp

--- choose-your-own-advenger/story.go
+++ choose-your-own-advenger/story.go
@@ -5,6 +5,28 @@ import (
 	"io"
 )
 
+var defaultHandlerTemp = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+      {{range .Option}}
+      <li><a href="\{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+    </ul>
+  </body>
+</html>`
+
 func JsonStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
 	var story Story
