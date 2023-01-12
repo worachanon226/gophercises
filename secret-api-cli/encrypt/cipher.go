780fb021c47c312c1bcd54e46e8030a428779ebd
Amaz <worachanon226@hotmail.com>
2023-01-12 20:22:16 +0700

    Update secret-api-cli

--- secret-api-cli/encrypt/cipher.go
+++ secret-api-cli/encrypt/cipher.go
@@ -11,30 +11,58 @@ import (
 	"io"
 )
 
-func Encrypt(key, plaintext string) (string, error) {
+func encryptStream(key string, iv []byte) (cipher.Stream, error) {
 	block, err := newCipherBlock(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
+	return cipher.NewCFBEncrypter(block, iv), nil
+}
 
+func Encrypt(key, plaintext string) (string, error) {
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
 		return "", err
 	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plaintext))
-
-	return fmt.Sprintf("%x", ciphertext), nil
-}
-
-func Decrypt(key, cipherHex string) (string, error) {
-	block, err := newCipherBlock(key)
+	stream, err := encryptStream(key, iv)
 	if err != nil {
 		return "", err
 	}
 
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plaintext))
+	return fmt.Sprintf("%x", ciphertext), nil
+}
+
+func EncryptWriter(key string, w io.Writer) (*cipher.StreamWriter, error) {
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	stream, err := encryptStream(key, iv)
+	if err != nil {
+		return nil, err
+	}
+	n, err := w.Write(iv)
+	if n != len(iv) || err != nil {
+		return nil, errors.New("encrypt: unable to write full iv to writer")
+	}
+	return &cipher.StreamWriter{
+		S: stream,
+		W: w,
+	}, nil
+}
+
+func decryptStream(key string, iv []byte) (cipher.Stream, error) {
+	block, err := newCipherBlock(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return cipher.NewCFBDecrypter(block, iv), nil
+}
+
+func Decrypt(key, cipherHex string) (string, error) {
 	ciphertext, err := hex.DecodeString(cipherHex)
 	if err != nil {
 		return "", err
@@ -46,8 +74,10 @@ func Decrypt(key, cipherHex string) (string, error) {
 
 	iv := ciphertext[:aes.BlockSize]
 	ciphertext = ciphertext[aes.BlockSize:]
-
-	stream := cipher.NewCFBDecrypter(block, iv)
+	stream, err := decryptStream(key, iv)
+	if err != nil {
+		return "", nil
+	}
 	stream.XORKeyStream(ciphertext, ciphertext)
 
 	return string(ciphertext), nil
--- secret-api-cli/vault.go
+++ secret-api-cli/vault.go
@@ -1,40 +1,119 @@
 package secret
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync"
 
 	cipher "worachanon226/gophercises/secret-api-cli/encrypt"
 )
 
-func Memory(encodingKey string) Vault {
-	return Vault{
+func File(encodingKey, filepath string) *Vault {
+	return &Vault{
 		encodingKey: encodingKey,
+		filepath:    filepath,
 		keyVaules:   make(map[string]string),
 	}
 }
 
 type Vault struct {
 	encodingKey string
+	filepath    string
+	mutex       sync.Mutex
 	keyVaules   map[string]string
 }
 
-func (v *Vault) Get(key string) (string, error) {
-	hex, ok := v.keyVaules[key]
-	if !ok {
-		return "", errors.New("secret: no value for that key")
-	}
-	ret, err := cipher.Decrypt(v.encodingKey, hex)
+func (v *Vault) loadKeyValues() error {
+	f, err := os.Open(v.filepath)
 	if err != nil {
-		return "", err
+		v.keyVaules = make(map[string]string)
+		return nil
 	}
-	return ret, nil
-}
+	defer f.Close()
 
-func (v *Vault) Set(key, value string) error {
-	encryptedValue, err := cipher.Encrypt(v.encodingKey, value)
+	var sb strings.Builder
+	_, err = io.Copy(&sb, f)
+	if err != nil {
+		return nil
+	}
+
+	decryptedJSON, err := cipher.Decrypt(v.encodingKey, sb.String())
+	if err != nil {
+		return nil
+	}
+
+	r := strings.NewReader(decryptedJSON)
+	dec := json.NewDecoder(r)
+	err = dec.Decode(&v.keyVaules)
 	if err != nil {
 		return err
 	}
-	v.keyVaules[key] = encryptedValue
 	return nil
+}
+
+func (v *Vault) readKeyValues(r io.Reader) error {
+	dec := json.NewDecoder(r)
+	return dec.Decode(&v.keyVaules)
+}
+
+func (v *Vault) writeKeyValues(w io.Writer) error {
+	enc := json.NewEncoder(w)
+	return enc.Encode(v.keyVaules)
+}
+
+func (v *Vault) saveKeyValues() error {
+	var sb strings.Builder
+	enc := json.NewEncoder(&sb)
+	err := enc.Encode(v.keyVaules)
+	if err != nil {
+		return err
+	}
+
+	encryptedJSON, err := cipher.Encrypt(v.encodingKey, sb.String())
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(v.filepath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+	_, err = fmt.Fprint(f, encryptedJSON)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (v *Vault) Get(key string) (string, error) {
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
+	err := v.loadKeyValues()
+	if err != nil {
+		return "", err
+	}
+	value, ok := v.keyVaules[key]
+	if !ok {
+		return "", errors.New("secret: no value for that key")
+	}
+	return value, nil
+}
+
+func (v *Vault) Set(key, value string) error {
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
+	err := v.loadKeyValues()
+	if err != nil {
+		return err
+	}
+	v.keyVaules[key] = value
+	err = v.saveKeyValues()
+	return err
 }
