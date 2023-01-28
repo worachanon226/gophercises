1368016f619c3bd399d1638a5c9bd80a21237eed
Amaz <worachanon226@hotmail.com>
2023-01-28 09:49:59 +0700

    add quick highlighting

---
+++ recover-middleware-sourcecode/go.mod
@@ -0,0 +1,8 @@
+module worachanon226/gophercises/rms
+
+go 1.18
+
+require (
+	github.com/alecthomas/chroma v0.10.0 // indirect
+	github.com/dlclark/regexp2 v1.8.0 // indirect
+)
--- recover-middleware-sourcecode/main.go
+++ recover-middleware-sourcecode/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"runtime/debug"
+
+	"github.com/alecthomas/chroma/quick"
 )
 
 func main() {
@@ -17,7 +18,7 @@ func main() {
 	mux.HandleFunc("panic-after/", panicAfterDemo)
 	mux.HandleFunc("/", hello)
 	mux.HandleFunc("/debug/", sourceCodeHandler)
-	log.Fatal(http.ListenAndServe(":3000", recoverMw(mux, true)))
+	log.Fatal(http.ListenAndServe(":3000", devMw(mux)))
 }
 
 func sourceCodeHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,91 +26,48 @@ func sourceCodeHandler(w http.ResponseWriter, r *http.Request) {
 	file, err := os.Open(path)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	io.Copy(w, file)
+	b := bytes.NewBuffer(nil)
+	_, err = io.Copy(b, file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_ = quick.Highlight(w, b.String(), "go", "html", "monokai")
 
 }
 
-func recoverMw(app http.Handler, dev bool) http.HandlerFunc {
+func devMw(app http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
 				log.Println(err)
 				stack := debug.Stack()
 				log.Println(string(stack))
-				if !dev {
-					http.Error(w, "Something went wrong :(", http.StatusInternalServerError)
-					return
-				}
 				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprintf(w, "<h1>panic: %s</h1><pre>%s<pre>", err, string(stack))
+				fmt.Fprintf(w, "<h1>panic: %v</h1><pre>%s</pre>", err, string(stack))
 			}
 		}()
-
-		nw := &responseWriter{ResponseWriter: w}
-		app.ServeHTTP(nw, r)
-		nw.flush()
+		app.ServeHTTP(w, r)
 	}
 }
 
-type responseWriter struct {
-	http.ResponseWriter
-	writes [][]byte
-	status int
-}
-
-func (rw *responseWriter) Write(b []byte) (int, error) {
-	rw.writes = append(rw.writes, b)
-	return len(b), nil
-}
-
-func (rw *responseWriter) WriteHeader(statusCode int) {
-	rw.status = statusCode
-}
-
-func (rw *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
-	hijaker, ok := rw.ResponseWriter.(http.Hijacker)
-	if !ok {
-		return nil, nil, fmt.Errorf("the ResponseWriter doesn't support the Hijacker interface")
-	}
-
-	return hijaker.Hijack()
-}
-
-func (rw *responseWriter) Flush() {
-	flusher, ok := rw.ResponseWriter.(http.Flusher)
-	if !ok {
-		return
-	}
-	flusher.Flush()
-}
-
-func (rw *responseWriter) flush() error {
-	if rw.status != 0 {
-		rw.ResponseWriter.WriteHeader(rw.status)
-	}
-	for _, write := range rw.writes {
-		_, err := rw.ResponseWriter.Write(write)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func panicDemo(_ http.ResponseWriter, _ *http.Request) {
+func panicDemo(w http.ResponseWriter, r *http.Request) {
 	funcThatPanics()
 }
 
-func panicAfterDemo(w http.ResponseWriter, _ *http.Request) {
+func panicAfterDemo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello!</h1>")
+	funcThatPanics()
 }
 
 func funcThatPanics() {
 	panic("Oh no!")
 }
 
-func hello(w http.ResponseWriter, _ *http.Request) {
+func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1>Hello!</h1>")
 }
